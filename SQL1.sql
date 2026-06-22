f744b4ad186c3d33279226ee2b7ab3cb22c1f32d
olzlkor <7714jihyo@gmail.com>
2026-06-22 16:32:54 +0900

    Update SQL1.sql

--- SQL1.sql
+++ SQL1.sql
@@ -1,31 +1,39 @@
-create database my_shop; -- 데이터 베이스 생성 (create database 이름;)
-use my_shop; -- 사용
-
--- 테이블 정의 : 필드 => 상품ID, 상품명, 가격, 재고 수량, 출시일
-create table sample ( -- 관계형 데이터베이스의 표 생성
-	pro_id int primary key, -- 기본키 
-    p_name char(100), -- varchar: 가변 길이, 최대 100
+create database my_shop;
+use my_shop;
+-- 테이블정의(설계) : 필드=> 상품id, 상품이름, 가격, 재고수량, 출시일
+create table sample (
+   pro_id int primary key,
+    p_name varchar(100),
     price int,
     quan int,
     re_date date
-);
-desc sample; -- 테이블 구조 확인
-show databases; -- 데이터 베이스 보기
-show tables; -- 테이블 보기
+    );
+desc sample;
+show databases;
+show tables;
+drop table sample;
+drop database my_shop;
 
--- c(입력):insert
+-- CRUD(넣고/읽고/수정/삭제) : 기본작업
+
 insert into sample (pro_id, p_name, price, quan, re_date)
-value (1, '새우깡', 3000, 100, '2026-5-3');
+value (1,'새우깡',3000,100, '2026-5-3');
+
+-- r(읽기) : select
+select * from sample;
 insert into sample (pro_id, p_name, price, quan, re_date)
-value (2,'양파링',2500,300,'2026-04-01');
+value (2, '양파링',2500,300, '2026-4-1');
+select * from sample;
 
--- r(읽기): select
-select * from sample my_shop;
-select pro_id, p_name from sample;
+-- u(갱신,수정) : update
 
--- u(갱신,수정): update
 update sample set price = 5000 where pro_id=1;
-update sample set quan = 1000 where pro_id=2;
+select * from sample;
 
--- d(삭제): delete
-delete from sample 
+-- 양파링 수량 1000개 수정
+update sample set quan = 1000 where pro_id=2;
+select * from sample;
+
+-- d(삭제) : delete
+delete from sample where pro_id=2;
+select * from sample;
